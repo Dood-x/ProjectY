570468971979923c70518cf97f642c5c2550a231
Dood-x <Dood-x@users.noreply.github.com>
2019-12-10 11:52:30 +0100

    first push

---
+++ PlatformerHomework/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ThirdPersonCameraWithLockOn;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController cc;
+    public ThirdPersonCamera camScript;
+    private Animator animator;
+
+    private Camera cam;
+
+    public float walkSpeed = 1.5f;
+    public float runSpeed = 6f;
+    public float rotSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+		animator = GetComponent<Animator> ();
+
+        if (camScript != null)
+        {
+            cam = camScript.gameObject.GetComponent<Camera>();
+        }
+        else
+        {
+            cam = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float animSpeed = Mathf.Max(Mathf.Abs(h), Mathf.Abs(v));
+
+        animator.SetFloat("Speed", animSpeed);
+
+        if (animSpeed > 0.1f)
+        {
+            // direction on the XZ plane determined by the inputs
+            Vector3 moveDirection = new Vector3(h, 0f, v);
+            // make the direction value lie on a unit circle
+            moveDirection.Normalize();
+            // the strength of the movement is as big as the highest input
+            moveDirection *= Mathf.Max(Mathf.Abs(h), Mathf.Abs(v));
+
+            // buffer, input should exceed 0.1 for the character to move
+            if (animSpeed < 0.1)
+            {
+                moveDirection = Vector3.zero;
+            }
+
+
+            // rotate the direction by the cameras local y rotation
+            moveDirection = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0) * moveDirection;
+
+            // we linearly interpolate between the walk and run speeds using the animation factor thats in the 0-1 interval
+            float movementSpeed = Mathf.Lerp(walkSpeed, runSpeed, animSpeed);
+
+            moveDirection *= Time.deltaTime * movementSpeed;
+
+            // move the character
+            cc.Move(moveDirection);
+
+            //turn the movement direction into a rotation
+            Quaternion rotation = Quaternion.LookRotation(moveDirection.normalized);
+            // rotate the characetr in the direction of movement
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, rotSpeed*Time.deltaTime);
+        }
+    }
+}
---
+++ PlatformerHomework/Assets/ThirdPersonCameraWithLockOn/Assets/Scripts/FadeObject.cs
@@ -0,0 +1,194 @@
+﻿/**
+	Copyright (C) 2019 NyangireWorks. All Rights Reserved.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+namespace ThirdPersonCameraWithLockOn
+{
+
+	public class FadeObject : MonoBehaviour {
+
+		private float opacity = 1f;
+
+		//Renderer renderer;
+
+		private Color c;
+
+		private float fadeOutSpeed = 10f;
+		public float FadeOutSpeed
+		{
+			get { return fadeOutSpeed; }
+			set { 
+					if( value < 0f)
+					{
+						fadeOutSpeed = 0f;
+					}
+					else
+					{
+						fadeOutSpeed = value; 
+					}
+				}
+		}
+
+		private float fadeInSpeed = 10f;
+		public float FadeInSpeed
+		{
+			get { return fadeInSpeed; }
+			set { 
+					if( value < 0f)
+					{
+						fadeInSpeed = 0f;
+					}
+					else
+					{
+						fadeInSpeed = value; 
+					}
+				}
+		}
+
+		private Shader fadeShader;
+		public Shader FadeShader
+		{
+			get { return fadeShader; }
+			set { fadeShader = value; }
+		}
+
+		private float targetTransparency;
+
+		private float originalTransparency;
+
+		private Material originalMaterial;
+		private Shader originalShader;
+		//private BlendMode originalRenderMode;
+
+		private bool fadeIn = true;
+
+		private bool debugOn = false;
+		public bool DebugOn 
+		{
+			get { return debugOn; }
+			set { debugOn = value; }
+		}
+
+		// Use this for initialization
+		void Start () {
+			//renderer = this.GetComponent<Renderer> ();
+
+			originalMaterial = GetComponent<Renderer>().sharedMaterial;
+			Material mat = GetComponent<Renderer>().material;
+			originalTransparency = mat.color.a;
+
+			if(fadeShader)
+			{
+				originalShader = mat.shader;
+				mat.shader = fadeShader;
+			}
+			
+			// mat.SetFloat("_Mode", 2);
+			// mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+			// mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+			// mat.SetInt("_ZWrite", 0);
+			// mat.DisableKeyword("_ALPHATEST_ON");
+			// mat.EnableKeyword("_ALPHABLEND_ON");
+			// mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+			// mat.renderQueue = 3000;
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			//FadeIn
+			if(fadeIn)
+			{
+				if(fadeInSpeed == 0)
+				{
+					opacity = targetTransparency;
+				}
+				else
+				{
+					opacity -= ((1.0f-targetTransparency)*Time.deltaTime) * fadeInSpeed;	
+				}
+				//reached target transparency
+				if(opacity <= targetTransparency)
+				{
+					opacity = targetTransparency;
+					fadeIn = false;
+				}
+				Color C = GetComponent<Renderer>().material.color;
+				C.a = opacity;
+				GetComponent<Renderer>().material.color = C;
+			}
+			
+			//FadeOut
+			else
+			{
+				if (opacity < originalTransparency)
+				{
+					Color C = GetComponent<Renderer>().material.color;
+					C.a = opacity;
+					GetComponent<Renderer>().material.color = C;
+				}
+				else
+				{
+					Material mat = GetComponent<Renderer>().material;
+
+					Color c = GetComponent<Renderer>().material.color;
+					c.a = originalTransparency;
+					
+
+					if(fadeShader)
+					{
+						mat.shader = originalShader;
+						mat.SetColor(originalShader.name, c);
+						//mat.EnableKeyword("_ALPHABLEND_ON");
+						GetComponent<Renderer>().material = originalMaterial;
+					}
+					else
+					{
+						mat.color = c;
+					}
+
+					
+
+					// mat.SetFloat("_Mode", 0);
+					// mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+					// mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+					// mat.SetInt("_ZWrite", 1);
+					// mat.DisableKeyword("_ALPHATEST_ON");
+					// mat.DisableKeyword("_ALPHABLEND_ON");
+					// mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+					// mat.renderQueue = 2000;
+
+					// And remove this script
+					Destroy(this);
+				}
+
+				if(fadeOutSpeed == 0)
+				{
+					opacity = originalTransparency;
+				}
+				else
+				{
+					opacity += ((1.0f-targetTransparency)*Time.deltaTime) * fadeOutSpeed;
+				}
+			}
+
+
+			if(debugOn)
+			{
+				UnityEngine.Debug.Log("opacity: " + opacity);
+			}
+		
+		}
+
+		public void SetTransparency(float newTP){
+			targetTransparency = newTP;
+			fadeIn = true;
+			//opacity = targetTransparency;
+		}
+
+	}
+}
