d786aabba9565c06b197a4240be7d1179edbc177
Dood-x <Dood-x@users.noreply.github.com>
2019-12-11 11:21:38 +0100

    ItemBobbing WIP

---
+++ PlatformerHomework/Assets/Scripts/ItemBobing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBobing : MonoBehaviour
+{
+    public float amplitude = 0.5f;
+    public float bobbingSpeed = 10f;
+    public Vector3 spinningSpeed;
+
+
+    Vector3 startPositon;
+    bool goDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPositon = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int sign = goDown ? -1 : 1;
+        transform.position += Vector3.up * bobbingSpeed * Time.deltaTime * sign;
+        if (Mathf.Abs(transform.position.y - startPositon.y) > amplitude)
+        {
+            transform.position = startPositon + Vector3.up * amplitude * sign;
+            goDown = !goDown;
+        }
+
+        transform.Rotate(spinningSpeed * Time.deltaTime, Space.World);
+    }
+}
