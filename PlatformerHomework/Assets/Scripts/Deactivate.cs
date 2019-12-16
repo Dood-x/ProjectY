7ccce3d08a0432857baf7c6744aad3a20c89838b
Dood-x <Dood-x@users.noreply.github.com>
2019-12-16 17:46:44 +0100

    Added spawner activate and deactivate prefab and spawner launch velocity

---
+++ PlatformerHomework/Assets/Scripts/Deactivate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivate : MonoBehaviour
+{
+    public GameObject objectToDeactivate;
+  
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            objectToDeactivate.SetActive(false);
+        }
+    }
+}
